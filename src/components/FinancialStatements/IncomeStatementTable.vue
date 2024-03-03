a4fd5fd8f286fac880087d5de8dde02afb7ac3d8
SiHoonChris <chrisleegpock@gmail.com>
2024-03-03 13:54:34 +0900

    Financial Statements - 2024.03.03

--- src/components/FinancialStatements/IncomeStatementTable.vue
+++ src/components/FinancialStatements/IncomeStatementTable.vue
@@ -18,9 +18,9 @@
           <tr><td>&emsp;Marginal Profit</td>         <td v-for="(d, i) in DATA" :key="i">{{DATA[i].NET_MARGINAL_PROFIT.toLocaleString()}}</td></tr>
           <tr><td>&emsp;Dividend, Interest</td>      <td v-for="(d, i) in DATA" :key="i">{{DATA[i].NET_DIVIDEND_INTEREST.toLocaleString()}}</td></tr>
           <tr><td>&emsp;&emsp;Total Net Income</td>  <td v-for="(d, i) in DATA" :key="i">{{DATA[i].TOTAL_NET_INCOME.toLocaleString()}}</td></tr>
-          <tr><td>Profit Margin Rate</td>            <td v-for="(d, i) in DATA" :key="i">{{DATA[i].PROFIT_MARGIN_RATE.toLocaleString()}}</td></tr>
-          <tr><td>ROE</td>                           <td v-for="(d, i) in DATA" :key="i">{{DATA[i].ROE.toLocaleString()}}</td></tr>
-          <tr><td>Net Income / Long-term Liab.</td>  <td v-for="(d, i) in DATA" :key="i">{{DATA[i].NET_INCOME_LONG_TERM_LIAB.toLocaleString()}}</td></tr>
+          <tr><td>Profit Margin Rate</td>            <td v-for="(d, i) in DATA" :key="i">{{DATA[i].PROFIT_MARGIN_RATE.toFixed(2).toLocaleString()}}%</td></tr>
+          <tr><td>ROE</td>                           <td v-for="(d, i) in DATA" :key="i">{{DATA[i].ROE.toFixed(2).toLocaleString()}}</td></tr>
+          <tr><td>Net Income / Long-term Liab.</td>  <td v-for="(d, i) in DATA" :key="i">{{DATA[i].NET_INCOME_LONG_TERM_LIAB.toFixed(4)}}</td></tr>
         </tbody>
       </table>
     </div>
