134da5255c48b0ce240f5bc6dc977f5e544f7027
SiHoonChris <chrisleegpock@gmail.com>
2024-03-10 18:09:24 +0900

    Detail - 2024.03.10

--- src/components/Detail/CandleChart.vue
+++ src/components/Detail/CandleChart.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="CandleChart">
     <div id="chart-helper">
-      <div id="asset-price"></div>
+      <div id="asset-price">
+        <span id="name">{{this.assetName}}</span>
+      </div>
       <div id="chart-tool"></div>
     </div>
     <div id="asset-chart"></div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      assetName: null
+    }
+  },
+  created() {
+    this.$store.commit('getAssetName', this.$route.params.ticker);
+    this.assetName = this.$store.state.assetName;
+  }
+}
+</script>
 
 <style scoped>
   #CandleChart {
@@ -25,9 +41,20 @@
     justify-content: space-between;
   }
   #asset-price {
-    width: 50%;
+    width: 50%; /*너비는 종목명에 따라 자유롭게 변동되어야 함*/
     height: 100%;
-    background: lightblue;
+    color: white;
+    border-right: 1px solid white;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+  }
+  #name {
+    font-size: 34px;
+    font-weight: bold;
+    padding-left: 0.6vw;
+    vertical-align: middle;
+    padding-bottom: 1%;
   }
   #chart-tool {
     width: 10%;
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    allAssetsData: null
+    allAssetsData: null,
+    assetName: null
   },
   getters: {
   },
   mutations: {
     setAllAssetsData(state, payload) {
       state.allAssetsData = payload;
+    },
+    getAssetName(state, payload) {
+      state.assetName = 
+      state.allAssetsData[state.allAssetsData.findIndex(e => e.TICKER === payload)].NAME;
     }
   },
   actions: {
