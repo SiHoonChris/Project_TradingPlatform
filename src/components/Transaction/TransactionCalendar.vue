9f0ef0451d8c3761124c6f6f59a1e6dd63807c4e
SiHoonChris <chrisleegpock@gmail.com>
2024-03-21 21:42:05 +0900

    Transaction - 2024.03.21

--- src/components/Transaction/TransactionCalendar.vue
+++ src/components/Transaction/TransactionCalendar.vue
@@ -3,13 +3,13 @@
     <div id="from-calendar">
       <p>FROM</p>
       <div id="select-from">
-        <flat-pickr v-model="date" :config="config"></flat-pickr>
+        <flat-pickr v-model="date_from" :config="config_from"></flat-pickr>
       </div>
     </div>
     <div id="to-calendar">
       <p>TO</p>
       <div id="select-to">
-        <flat-pickr v-model="date" :config="config"></flat-pickr>
+        <flat-pickr v-model="date_to" :config="config_to"></flat-pickr>
       </div>
     </div>
   </div>
@@ -23,14 +23,33 @@ export default {
   components: { flatPickr },
   data() {
     return {
-      date: null,
-      config: {
+      date_from: null,
+      config_from: {
         inline: true,
         enableTime: false,
+      },
+      date_to: null,
+      config_to: {
+        inline: true,
+        enableTime: false,
+        minDate: null
       }
     }
   },
   created(){
+    },
+  mounted(){
+    const WIDTH = window.getComputedStyle(document.querySelector("#select-from")).width;
+    const HEIGHT = window.getComputedStyle(document.querySelector("#select-from")).height;
+    for(const E of document.querySelectorAll(".flatpickr-calendar")){
+      E.style.width = WIDTH;
+      E.style.height = HEIGHT;
+    }
+  },
+  watch: {
+    date_from: function(val){
+      this.config_to.minDate = val;
+    }
   }
 }
 </script>
@@ -45,7 +64,7 @@ export default {
   }
   #from-calendar, #to-calendar {
     width: 100%;
-    height: 45%;
+    height: 50%;
   }
   p {
     margin: 0;
@@ -63,8 +82,8 @@ export default {
   .flatpickr-input {
     display: none;
   }
-  .flatpickr-calendar {
-    width: 331.775px;
-    height: 100%;
+  .flatpickr-calendar{
+    width: 330px;
+    height: 260px;
   }
 </style>
