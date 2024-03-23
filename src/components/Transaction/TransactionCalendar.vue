cee7dc418c5e2d6458361f3bce5d2493da31a4df
SiHoonChris <chrisleegpock@gmail.com>
2024-03-23 22:13:52 +0900

    Transaction - 2024.03.23

--- src/components/Transaction/TransactionCalendar.vue
+++ src/components/Transaction/TransactionCalendar.vue
@@ -23,27 +23,77 @@ export default {
   components: { flatPickr },
   data() {
     return {
-      date_from: null,
+      date_from: null, // 첫 화면 에서는 Today임
       config_from: {
         inline: true,
         enableTime: false,
+        maxDate: "today"
       },
-      date_to: null,
+      date_to: null, // 첫 화면 에서는 Today임
       config_to: {
         inline: true,
         enableTime: false,
-        minDate: null
-      }
+        minDate: null,
+        maxDate: "today"
+      },
+      WIDTH: null,
+      HEIGHT: null
     }
   },
-  created(){
-    },
   mounted(){
-    const WIDTH = window.getComputedStyle(document.querySelector("#select-from")).width;
-    const HEIGHT = window.getComputedStyle(document.querySelector("#select-from")).height;
+    this.WIDTH = window.getComputedStyle(document.querySelector("#select-from")).width;
+    this.HEIGHT = window.getComputedStyle(document.querySelector("#select-from")).height;
+    
     for(const E of document.querySelectorAll(".flatpickr-calendar")){
-      E.style.width = WIDTH;
-      E.style.height = HEIGHT;
+      E.style.width = this.WIDTH;
+      E.style.height = this.HEIGHT;
+      E.style.top = '0px';
+      E.style.bottom = '0px';
+    }
+    for(const E of document.querySelectorAll(".flatpickr-current-month")){E.style.paddingTop = '1px';}
+    for(const E of document.querySelectorAll(".flatpickr-days")){E.style.width = this.WIDTH;}
+    for(const E of document.querySelectorAll(".dayContainer")){
+      E.style.height = 
+        String(
+          this.HEIGHT.replace('px', '') 
+          - window.getComputedStyle(document.querySelectorAll(".flatpickr-weekdays")[0]).height.replace('px', '')
+          - window.getComputedStyle(document.querySelectorAll(".flatpickr-months")[0]).height.replace('px', '')
+        )+'px';
+      E.style.width = this.WIDTH;
+      E.style.maxWidth = this.WIDTH;
+    }
+    for(const E of document.querySelectorAll(".dayContainer .flatpickr-day")){
+      const rowsNum = document.querySelectorAll(".dayContainer .flatpickr-day").length > 35 ? 6 : 5;
+      E.style.height = String(
+        window.getComputedStyle(document.querySelectorAll(".dayContainer")[0]).height.replace('px', '') / rowsNum
+      )+'px';
+      E.style.lineHeight = String(
+        window.getComputedStyle(document.querySelectorAll(".dayContainer")[0]).height.replace('px', '') / rowsNum
+      )+'px';
+    }
+    for(const E of document.querySelectorAll(".flatpickr-months span")){E.style.paddingTop = '3px';}
+    for(const E of document.querySelectorAll(".flatpickr-weekdays")){E.style.width = this.WIDTH;}
+  },
+  updated(){
+    for(const E of document.querySelectorAll(".flatpickr-days")){E.style.width = this.WIDTH;}
+    for(const E of document.querySelectorAll(".dayContainer")){
+      E.style.height = 
+        String(
+          this.HEIGHT.replace('px', '') 
+          - window.getComputedStyle(document.querySelectorAll(".flatpickr-weekdays")[0]).height.replace('px', '')
+          - window.getComputedStyle(document.querySelectorAll(".flatpickr-months")[0]).height.replace('px', '')
+        )+'px';
+      E.style.width = this.WIDTH;
+      E.style.maxWidth = this.WIDTH;
+    }
+    for(const E of document.querySelectorAll(".dayContainer .flatpickr-day")){
+      const rowsNum = document.querySelectorAll(".dayContainer .flatpickr-day").length > 35 ? 6 : 5;
+      E.style.height = String(
+        window.getComputedStyle(document.querySelectorAll(".dayContainer")[0]).height.replace('px', '') / rowsNum
+      )+'px';
+      E.style.lineHeight = String(
+        window.getComputedStyle(document.querySelectorAll(".dayContainer")[0]).height.replace('px', '') / rowsNum
+      )+'px';
     }
   },
   watch: {
@@ -69,7 +119,6 @@ export default {
   p {
     margin: 0;
     padding-left: 2%;
-    padding-bottom: 1%;
     height: 9%;
     color: white;
     font-weight: bold;
@@ -77,13 +126,9 @@ export default {
   #select-from, #select-to {
     width: 100%;
     height: 90%;
-    border: 1px solid gray;
   }
   .flatpickr-input {
     display: none;
   }
-  .flatpickr-calendar{
-    width: 330px;
-    height: 260px;
-  }
+  
 </style>
--- src/components/Transaction/TransactionTable.vue
+++ src/components/Transaction/TransactionTable.vue
@@ -1,33 +1,53 @@
 <template>
   <div id="transaction-table">
-    
+
     <div id="table-sort">
       <select>
-        <option>All</option>
-        <option>Deposit</option>
-        <option>Withdraw</option>
-        <option>Buy</option>
-        <option>Sell</option>
-        <option>Dividend</option>
+        <option v-for="(d, i) in transaction_list" :key="i">{{d}}</option>
       </select>
       <input type='text' placeholder="Search"/>
     </div>
     
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>NAME</th>
-          <th>Transaction</th>
-          <th>Detail</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td><td></td><td></td><td></td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="transaction-list">
+      <div id="transaction-list-thead">
+        <table>
+          <thead>
+            <tr>
+              <th rowspan="2">Date</th>
+              <th>Transaction</th>
+              <th rowspan="2">Name</th>
+              <th>Price or FX-Rate</th>
+              <th>Currency</th>
+            </tr>
+            <tr>
+              <th>Detail</th>
+              <th>Amount</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div id="transaction-list-tbody">
+        <table>
+          <tbody>
+            <template v-for="(e,i) in Data" :key="i">
+              <tr>
+                <td rowspan="2">{{e.Date}}</td>
+                <td>{{e.Transaction}}</td>
+                <td rowspan="2">{{e.Name}}</td>
+                <td>{{e.Transaction !== "Banking"  ?  e.Price === 0 ? e.FX_Rate.toFixed(4) : e.Price.toFixed(4)  :  0}}</td>
+                <td>{{e.Currency}}</td>
+              </tr>
+              <tr>
+                <td>{{e.Detail}}</td>
+                <td>{{e.Transaction !== 'Banking' ? Number(e.Amount).toLocaleString() : 0}}</td>
+                <td>{{Number(e.Value).toLocaleString()}}</td>
+              </tr>
+            </template>
+          </tbody>
+        </table>
+      </div>
+    </div>
     
     <div id="pagination">
       <p>
@@ -42,11 +62,40 @@
         <span>9</span>
         <span>10</span>
       </p>
-      <span>[ Total : XXX ]</span>
+      <span>[ Total : {{this.Data.length}} ]</span>
     </div>
 
   </div>
 </template>
+
+<script>
+import SampleTransactionList from "@/assets/SampleTransactionList.json"
+
+export default {
+  data() {
+    return {
+      Data: null,
+      Settlement: null,
+      transaction_list: [ 
+        "All", 
+        "Banking", 
+        "Exchange", 
+        "Trading",
+        "Deposit",
+        "Withdraw",
+        "Dividend",
+        "Exchange-Buy",
+        "Exchange-Sell",
+        "Buy",
+        "Sell"
+      ]
+    }
+  },
+  created(){
+    this.Data = SampleTransactionList;
+  }
+}
+</script>
 
 <style scoped>
   #transaction-table {
@@ -80,23 +129,64 @@
     margin-bottom: 1%;
   }
 
-  table {
+  #transaction-list {
     width: 100%;
     height: 85%;
+  }
+  #transaction-list-thead { 
+    height: 12%; 
+    border-left: 1px solid gray;
+    border-right: 1px solid gray;
+    border-top: 1px solid gray;
+  }
+  #transaction-list-thead table {
+    width: 100%;
+    height: 100%;
     border-collapse: collapse;
     color: white;
   }
-  thead {
-    height: 12%;
+  #transaction-list-tbody { 
+    height: calc(88% - 0.4% * 2); 
+    border-left: 1px solid gray;
+    border-right: 1px solid gray;
+    border-bottom: 1px solid gray;
+    overflow-y: scroll;
   }
-  th:nth-child(1) { width: 10%; }
-  th:nth-child(2) { width: 14%; }
-  th:nth-child(3) { width: 16%; }
-  th:nth-child(4) { width: 60%; }
-  th, td {
-    padding: 0.4% 1.4%;
-    border: 1px solid gray;
+  #transaction-list-tbody table {
+    width: 100%;
+    height: 100%;
+    border-collapse: collapse;
+    color: white;
   }
+  th {
+    padding: 0.4% 1.0%;
+    border-bottom: 1px solid gray;
+  }
+  th:nth-child(1) { width: 12%; border-right: 1px solid gray; }
+  th:nth-child(2) { width: 20%; border-right: 1px solid gray; }
+  th:nth-child(3) { width: 20%; border-right: 1px solid gray; }
+  th:nth-child(4) { width: 24%; border-left: 1px solid gray; }
+  th:nth-child(5) { width: 24%; border-left: 1px solid gray; }
+  tr:nth-child(odd) > th:nth-child(1) , tr:nth-child(odd) > th:nth-child(3) { vertical-align: middle; }
+  tr:nth-child(odd) > th:nth-child(2) , tr:nth-child(odd) > th:nth-child(4) , tr:nth-child(odd) > td:nth-child(5) { vertical-align: top; }
+  tr:nth-child(even) > th { vertical-align: top; }
+  #transaction-list-thead > table > thead > tr:nth-child(2) > th:nth-child(3) { border-right: none; }
+  td {
+    padding: 0.4% 1.0%;
+    border-bottom: 1px solid gray;
+  }
+  td:nth-child(1) { width: 12%; border-right: 1px solid gray; }
+  td:nth-child(2) { width: 20%; border-right: 1px solid gray; }
+  td:nth-child(3) { width: 20%; border-right: 1px solid gray; }
+  td:nth-child(4) { width: 24%; border-left: 1px solid gray; }
+  td:nth-child(5) { width: 23.2%; border-left: 1px solid gray; }
+  tr:nth-child(odd) > td:nth-child(1) , tr:nth-child(odd) > td:nth-child(3) { vertical-align: middle; text-align: center; }
+  tr:nth-child(odd) > td:nth-child(2) , tr:nth-child(odd) > td:nth-child(4) , tr:nth-child(odd) > td:nth-child(5) { vertical-align: top; }
+  tr:nth-child(even) > td { vertical-align: top; }
+  tr:nth-child(odd) > td:nth-child(4) , tr:nth-child(odd) > td:nth-child(5) , 
+  tr:nth-child(even) > td:nth-child(2) , tr:nth-child(even) > td:nth-child(3) { text-align: right; }
+  #transaction-list-tbody > table > tbody > tr > td:nth-child(5) { border-right: 1px solid gray; }
+  tr:nth-child(4n+1) > td , tr:nth-child(4n+2) > td {background:#1a1a1a;}
 
   #pagination {
     width: 100%;
@@ -121,7 +211,7 @@
     font-weight: bold;
     color: whitesmoke;
   }
-  #pagination > p > span:last-child {
+  #pagination > p > span:nth-child(10) {
     content: '>';
   }
   #pagination > span {
