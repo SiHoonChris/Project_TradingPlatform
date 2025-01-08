ad96359da73efbe6aa1de2d253b4c0bf9702d284
SiHoonChris <chrisleegpock@gmail.com>
2025-01-08 20:00:12 +0900

    [2025.01.08] Home

--- src/components/Home/ChartComponent.vue
+++ src/components/Home/ChartComponent.vue
@@ -1,14 +1,46 @@
 <template>
-  <div id="component-chart"></div>
+  <div id="component-chart" class="tradingview-widget-container">
+    <div :id="listPart"></div>
+    <div :id="chartPart"></div>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
-
+      return {
+        symbol: "NASDAQ:AAPL",
+        interval: "D",
+        listPart: "watch-list",
+        chartPart: "tradingview-widget" 
+      }
     },
-    methods: {
+    mounted () {
+      this.$nextTick(() => {
+        const chartContainer = document.getElementById(this.chartPart);
+        const [chartWidth, chartHeight] = [chartContainer.offsetWidth, chartContainer.offsetHeight];
 
+        if (window.TradingView) {
+          new window.TradingView.widget({
+            symbol: this.symbol,
+            container_id: this.chartPart,
+            width: chartWidth,
+            height: chartHeight,
+            theme: "dark",
+            interval: this.interval,
+            timezone: "Etc/UTC",
+            style: "1",
+            locale: "en",
+            toolbar_bg: "#111111",
+            enable_publishing: false,
+            allow_symbol_change: true,
+            studies: [
+              "BB@tv-basicstudies", 
+              "IchimokuCloud@tv-basicstudies"
+            ]
+          });
+        }
+      });
     }
   }
 </script>
